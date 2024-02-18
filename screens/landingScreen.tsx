a8afe33ec8dd3cffa5182281a66585261b53da2d
Mohamed Siraj <siraj@oganro.com>
2024-02-19 01:31:44 +0530

    changes update

--- screens/landingScreen.tsx
+++ screens/landingScreen.tsx
@@ -1,4 +1,4 @@
-import { Dimensions, View, Text } from "react-native";
+import { Dimensions, View, Text, Image } from "react-native";
 import Carousel from 'react-native-reanimated-carousel';
 
 const LandingScreen = () => {
@@ -8,31 +8,30 @@ const LandingScreen = () => {
      */
     const width = Dimensions.get('window').width;
 
+    const images = [
+        require('../assets/img/1.jpg'),
+        require('../assets/img/2.jpg'),
+        require('../assets/img/3.jpg'),
+        require('../assets/img/4.jpg'),
+        require('../assets/img/5.jpg')
+    ];
+
     return (
         <View className="flex-1 items-center bg-white">
-            <Text className="font-bold text-2xl text-gray-600 mt-7">Explore</Text>
+            <Text className="font-bold text-2xl text-gray-600 mt-7 mb-3">Explore</Text>
             <Carousel
                 loop
                 width={width}
-                height={width / 3}
+                height={width / 2}
                 autoPlay={true}
-                data={[...new Array(6).keys()]}
+                data={images}
                 scrollAnimationDuration={1000}
-                onSnapToItem={(index) => console.log('current index:', index)}
-                renderItem={({ index }) => (
-                    <View
-                        style={{
-                            flex: 1,
-                            borderWidth: 1,
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <Text style={{ textAlign: 'center', fontSize: 30 }}>
-                            {index}
-                        </Text>
-                    </View>
+                onSnapToItem={(index) => {}}
+                renderItem={({ index, item } : any) => (
+                    <Image className="rounded" source={item} style={{ width: "100%", height: "100%" }}/>
                 )}
             />
+            <Text className="font-bold text-2xl text-gray-600 mt-7 mb-3 text-start">Explore</Text>
         </View>
     );
 };
